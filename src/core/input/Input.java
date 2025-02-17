31adcfdf481ae145241c7b1ee3a50495d1ae6a4b
OscarRequena <oscarjair_7@hotmail.com>
2025-02-17 19:29:34 +0100

    Se ha dado de Alta

---
+++ src/core/input/Input.java
@@ -0,0 +1,31 @@
+package core.input;
+
+import java.util.Scanner;
+
+public class Input {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int recibirParametroMenu() {
+        return sc.nextInt();
+    }
+
+    public static String recibirNombreCompleto() {
+        return sc.next();
+    }
+
+    public static int recibirEdad() {
+        return sc.nextInt();
+    }
+
+    public static String recibirFechaNacimiento() {
+        return sc.next();
+    }
+
+    public static String recibirNombreAsignatura() {
+        return sc.next();
+    }
+
+    public static double recibirNota() {
+        return sc.nextInt();
+    }
+}
---
+++ src/main/Crud_Estudiantes.java
@@ -0,0 +1,57 @@
+package main;
+
+import core.model.Estudiantes;
+import java.util.HashMap;
+
+public class Crud_Estudiantes {
+    public static int opcionMenu;
+   public final static int DAR_ALTA_ESTUDIANTE = 1;
+    final static int DAR_BAJA_ESTUDIANTE = 2;
+    final static int MODIFICAR_NOTA = 3;
+    final static int LISTADO_ESTUDIANTE = 4;
+   public final static int SALIR_PROGRAMA = 5;
+   public final static int NUMERO_ESTUDIANTES = 2;
+   public static HashMap<Integer, Estudiantes> estudiantes;
+    public static void main(String[] args) {
+        estudiantes = new HashMap<>();
+        boolean UsuarioQuiereSalir = false;
+        do {
+            boolean ValidadorIncorrecto = false;
+            do {
+                mostrarMenu();
+                opcionMenu = recibirParametroMenu();
+                ValidadorIncorrecto = validadorMenu();
+                if (!ValidadorIncorrecto){
+                    core.output.Output.mensajeError();
+                }
+            }while(!ValidadorIncorrecto);
+            if (opcionMenu == SALIR_PROGRAMA){
+                UsuarioQuiereSalir = true;
+            }else {
+                switch (opcionMenu){
+                    case DAR_ALTA_ESTUDIANTE:
+                        darAltaEstudiante();
+                        break;
+                    case LISTADO_ESTUDIANTE:
+                        break;
+                }
+            }
+        }while(!UsuarioQuiereSalir);
+    }
+
+    private static void darAltaEstudiante() {
+        altas.Altas.darAltaEstudiante();
+    }
+
+    private static int recibirParametroMenu() {
+        return core.input.Input.recibirParametroMenu();
+    }
+
+    private static boolean validadorMenu() {
+        return core.validador.Validador.validadorMenu(opcionMenu);
+    }
+
+    private static void mostrarMenu() {
+        core.output.Output.mostrarMenu();
+    }
+}
